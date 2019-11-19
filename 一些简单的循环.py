5afc2cb159875d67718769d5aff1317bcfe72d62
舒波 郭 <2420019491@qq.com>
2019-11-19 11:53:26 +0800

    添加项目文件。

---
+++ 一些简单的循环.py
@@ -0,0 +1,21 @@
+
+a,b=0,1
+while b<100:
+    print(b,end=' ')
+    a,b=b,a+b
+print(end='...\n')#斐波那契数列
+
+
+mun1=100
+if mun1:
+    print(mun1)
+#"mun1".strip()----这是取消空格的方法之一，strip取消左右两边的空格，lstrip取消左边，rstrip取消右边
+mun2=0
+if mun2:
+    print(mun2)
+
+mun3=23
+if mun3:
+    print(mun3)
+#去掉换行后空格的小方法：
+#eg：print（‘a'+‘\n’+'b'）
